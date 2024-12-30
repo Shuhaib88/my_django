31b3982bd181970c1caf25bf75b8592cb3af9ba5
shuhaibtuttushuhaib@gmail.com <shuhaibtuttushuhaib@gmail.com>
2024-12-30 09:12:39 +0530

    demo commit

--- mynew_django/settings.py
+++ mynew_django/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-2ce3mmmg!&1j#wd5g5_peyx^0glw1*@d28ze)(xx##s)j2&48$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app',  'mynew_django.vercel.app', '192.168.1.2', '127.0.0.1', 'localhost', '139.59.6.114']
+ALLOWED_HOSTS = ['192.168.1.2', '127.0.0.1', 'localhost', '68.183.91.111']
 
 # Application definition
 
